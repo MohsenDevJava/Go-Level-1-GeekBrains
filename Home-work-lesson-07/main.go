4a893bb56dbf7f696c76b37424781eefd4f7e603
mohsen-dev-go <92440669+mohsen-dev-go@users.noreply.github.com>
2022-05-16 12:50:38 +0300

    Home-work-lesson-07

---
+++ Home-work-lesson-07/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"bytes"
+	"io"
+)
+
+type MyBytesStr struct {
+	Storage []byte
+}
+
+type MyBytesInterFace interface {
+	Read(value byte) byte
+	Write(value byte) byte
+}
+
+func (my *MyBytesInterFace) Read(value byte) byte {
+
+	io.WriteString(value)
+}
+
+func main() {
+
+	bytes.Buffer{}
+}
